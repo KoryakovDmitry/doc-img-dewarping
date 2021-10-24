d3346e4282e1eebfac6eabcc6b693e3a30792cfd
DmitryKoryakov <dmitry.koryakov@initflow.com>
2021-10-24 12:28:28 +0300

    added lsd

---
+++ text_line_segmentation/line_segm.py
@@ -0,0 +1,35 @@
+import os
+import cv2
+import numpy as np
+from glob import glob
+import os.path as osp
+from tqdm import tqdm
+from PIL import Image
+
+
+path = "../test_imgs/33.jpg"
+# Read gray image
+img = cv2.imread(path, 0)
+
+# Create default parametrization LSD
+lsd = cv2.createLineSegmentDetector(0)
+
+# Detect lines in the image
+lines = lsd.detect(img)[0]  # Position 0 of the returned tuple are the detected lines
+
+# Draw detected lines in the image
+# drawn_img = lsd.drawSegments(img, lines)
+lengs = np.hypot(abs(lines[:, 0, 0] - lines[:, 0, 2]), abs(lines[:, 0, 1] - lines[:, 0, 3]))
+mean_leng = lengs.mean()
+
+drawn_img = img.copy()
+for i_ in lines:
+    i = i_[0]
+    start_point = (int(i[0]), int(i[1]))
+    end_point = (int(i[2]), int(i[3]))
+    leng = np.hypot(abs(start_point[0] - end_point[0]), abs(start_point[1] - end_point[1]))
+    if mean_leng < leng:
+        drawn_img = cv2.line(drawn_img, start_point, end_point, (0, 0, 255), 2)
+
+
+Image.fromarray(drawn_img).show()
