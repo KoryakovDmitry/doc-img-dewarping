4839b7863edcddc3e1a44542455d463bb4aea399
DmitryKoryakov <dmitry.koryakov@initflow.com>
2021-11-07 23:59:46 +0300

    upd

--- homo_warp_trans/homo_trans.py
+++ homo_warp_trans/homo_trans.py
@@ -179,7 +179,7 @@ def dewarp(image, pts_src, debug_plot):
                 2,
                 cv2.LINE_AA,
             )
-        Image.fromarray(img_plot[:, :, ::-1]).show()
+        # Image.fromarray(img_plot[:, :, ::-1]).show()
     #
     h, w = int(np.round(h)), int(np.round(w))
     un_warped = unwarp(image, np.float32(pts_src), np.float32(destination_pts))
@@ -212,22 +212,10 @@ def get_distance(ref, point):
     return math.hypot(x2 - x1, y2 - y1)
 
 
-def get_angle_between_two_lines(first_line, second_line):
-    first_line = Line(Point2D(first_line[0]), Point2D(first_line[1]))
-    second_line = Line(Point2D(second_line[0]), Point2D(second_line[1]))
-    return 180 - np.rad2deg(float(first_line.angle_between(second_line)))
-
-
 def get_angle_by_idx(angles, idx):
     for a in angles:
-        if idx == a[1][1]:
-            return a[0]
-
-
-def get_pt_by_idx(angles, idx):
-    for a in angles:
-        if idx == a[1][1]:
-            return a[2][0][1]
+        if idx == a[0][1]:
+            return a
 
 
 def get_max_poly4(idxs, points):
@@ -240,13 +228,16 @@ def get_max_poly4(idxs, points):
 class HomographyTrans:
     def __init__(self):
         self.segm = Segmentator()
-        self.tol_perc = 2.2058823529411766 * 0.01
-        # self.tol_perc = 1.7
+        # self.tol_perc = 2.2058823529411766 * 0.01
+        self.tol_perc = 5.0 * 0.01
+        self.tol_orig_perc = 2.5 * 0.01
 
     def inference(self, img: np.array, debug_plot=True):
-        tol = np.hypot(img.shape[0], img.shape[1]) * self.tol_perc
+        main_hypot = np.hypot(img.shape[0], img.shape[1])
+        tol = main_hypot * self.tol_perc
         points = self.segm.inference(img)
         points = group_points(points, tol=tol)
+        points = np.array(points)
 
         # rect_all = []
         # for pi1, pi2, pi3, pi4 in combinations(range(0, points.shape[0]), r=4):
@@ -254,7 +245,7 @@ class HomographyTrans:
         #     p = Polygon([p1, p2, p3, p4])
         #     rect_all.append((p.area, []))
         l_get_max_poly4 = lambda idxs: get_max_poly4(idxs, points=points)
-        idxs_ploy_max = max(
+        idxs_poly_max = max(
             combinations(range(0, points.shape[0]), r=4), key=l_get_max_poly4
         )
 
@@ -262,22 +253,181 @@ class HomographyTrans:
             h = plot_border_corrected_(img.copy(), points)
 
         if len(points) > 2:
-            points_max_poly_inside = points[[idxs_ploy_max]]
-            picks_clockwised = order_points_clockwise(points_max_poly_inside)
+            angles = []
 
             if debug_plot:
-                h = cv2.polylines(h, [picks_clockwised.astype(int)], True, color=(0, 255, 0), thickness=5)
+                img_plot = img.copy()
+
+            for prev_i in range(0, len(points) - 2):
+                prev_pt = points[prev_i]
+                now_pt = points[prev_i + 1]
+                next_pt = points[prev_i + 2]
+
+                first_line = [prev_pt, now_pt]
+                second_line = [now_pt, next_pt]
+
+                l_1 = np.array(first_line).astype(int)
+                l_2 = np.array(second_line).astype(int)
+
+                if debug_plot:
+                    img_plot = cv2.line(
+                        img_plot,
+                        tuple(l_1[0]),
+                        tuple(l_1[1]),
+                        (255, 0, 255),
+                        thickness=3,
+                    )
+                    img_plot = cv2.line(
+                        img_plot,
+                        tuple(l_2[0]),
+                        tuple(l_2[1]),
+                        (255, 0, 255),
+                        thickness=3,
+                    )
+
+                angles.append(
+                    (
+                        (prev_i, prev_i + 1, prev_i + 2),
+                        (first_line, second_line),
+                    )
+                )
+
+            prev_pt = points[prev_i + 1]
+            now_pt = points[prev_i + 2]
+            next_pt = points[0]
+
+            first_line = [prev_pt, now_pt]
+            second_line = [now_pt, next_pt]
+
+            l_1 = np.array(first_line).astype(int)
+            l_2 = np.array(second_line).astype(int)
+            if debug_plot:
+                img_plot = cv2.line(
+                    img_plot, tuple(l_1[0]), tuple(l_1[1]), (255, 255, 0), thickness=3
+                )
+                img_plot = cv2.line(
+                    img_plot, tuple(l_2[0]), tuple(l_2[1]), (255, 255, 0), thickness=3
+                )
+
+            angles.append(
+                (
+                    (prev_i + 1, prev_i + 2, 0),
+                    (first_line, second_line),
+                )
+            )
+
+            prev_pt = points[prev_i + 2]
+            now_pt = points[0]
+            next_pt = points[1]
+
+            first_line = [prev_pt, now_pt]
+            second_line = [now_pt, next_pt]
+
+            l_1 = np.array(first_line).astype(int)
+            l_2 = np.array(second_line).astype(int)
+
+            if debug_plot:
+                img_plot = cv2.line(
+                    img_plot, tuple(l_1[0]), tuple(l_1[1]), (0, 255, 255), thickness=3
+                )
+                img_plot = cv2.line(
+                    img_plot, tuple(l_2[0]), tuple(l_2[1]), (0, 255, 255), thickness=3
+                )
+
+            angles.append(((prev_i + 2, 0, 1), (first_line, second_line)))
+
+            poly_max_ang = [get_angle_by_idx(angles, idx) for idx in idxs_poly_max]
+
+            close_90_cluster_pts = points[[idxs_poly_max]]
+            picks_clockwised = order_points_clockwise(close_90_cluster_pts)
+            close_90_cluster_sorted = []
+            for pick in picks_clockwised:
+                c_i = np.argwhere(np.sum(close_90_cluster_pts == pick, axis=1) == 2)[0][
+                    0
+                ]
+                c = poly_max_ang[c_i]
+                close_90_cluster_sorted.append(c)
+
+            # change near pts (~ 90 deg)
+            for num, c in enumerate(close_90_cluster_sorted):
+                pts = c[1][0][1]
+                pts_l = c[1][0][0]
+                pts_r = c[1][1][1]
+
+                if num == 0:
+                    picks_clockwised[num][0] = np.min([pts_l[0], pts[0], pts_r[0]])
+                    picks_clockwised[num][1] = np.min([pts_l[1], pts[1], pts_r[1]])
+
+                elif num == 1:
+                    picks_clockwised[num][0] = np.max([pts_l[0], pts[0], pts_r[0]])
+                    picks_clockwised[num][1] = np.min([pts_l[1], pts[1], pts_r[1]])
+
+                elif num == 2:
+                    picks_clockwised[num][0] = np.max([pts_l[0], pts[0], pts_r[0]])
+                    picks_clockwised[num][1] = np.max([pts_l[1], pts[1], pts_r[1]])
+
+                elif num == 3:
+                    picks_clockwised[num][0] = np.min([pts_l[0], pts[0], pts_r[0]])
+                    picks_clockwised[num][1] = np.max([pts_l[1], pts[1], pts_r[1]])
+
+            if debug_plot:
+                h = cv2.polylines(
+                    h,
+                    [picks_clockwised.astype(int)],
+                    True,
+                    color=(0, 0, 255),
+                    thickness=5,
+                )
                 # Image.fromarray(h[:, :, ::-1]).show()
-                return h
+
+            dist_pt1 = np.hypot(
+                abs(picks_clockwised[0][0] - 0),
+                abs(picks_clockwised[0][1] - 0),
+            )
+            dist_pt2 = np.hypot(
+                abs(picks_clockwised[1][0] - img.shape[1]),
+                abs(picks_clockwised[1][1] - 0),
+            )
+            dist_pt3 = np.hypot(
+                abs(picks_clockwised[2][0] - img.shape[1]),
+                abs(picks_clockwised[2][1] - img.shape[0]),
+            )
+            dist_pt4 = np.hypot(
+                abs(picks_clockwised[3][0] - 0),
+                abs(picks_clockwised[3][1] - img.shape[0]),
+            )
+            tol_orig = self.tol_orig_perc * main_hypot
+            if dist_pt1 < tol_orig:
+                picks_clockwised[0][0] = 0
+                picks_clockwised[0][1] = 0
+            if dist_pt2 < tol_orig:
+                picks_clockwised[1][0] = img.shape[1]
+                picks_clockwised[1][1] = 0
+            if dist_pt3 < tol_orig:
+                picks_clockwised[2][0] = img.shape[1]
+                picks_clockwised[2][1] = img.shape[0]
+            if dist_pt4 < tol_orig:
+                picks_clockwised[3][0] = 0
+                picks_clockwised[3][1] = img.shape[0]
+
+            if debug_plot:
+                h = cv2.polylines(
+                    h,
+                    [picks_clockwised.astype(int)],
+                    True,
+                    color=(0, 255, 0),
+                    thickness=5,
+                )
+                # Image.fromarray(h[:, :, ::-1]).show()
 
             un_warped, cropped = dewarp(
                 image=img, pts_src=picks_clockwised, debug_plot=debug_plot
             )
             if debug_plot:
-                Image.fromarray(cropped[:, :, ::-1]).show()
-            return cropped
-
-        return None
+                # Image.fromarray(cropped[:, :, ::-1]).show()
+                pass
+            return cropped, h
+        return img, None
 
 
 if __name__ == "__main__":
@@ -293,14 +443,23 @@ if __name__ == "__main__":
     # anns_dir = osp.join(base, "test_anns_4_pts/")
     anns_dir = osp.join(base, "test_anns/")
     for im_path in imgs:
+        # if "rot_invoices(apr1-10)-001_00113086_kAePHqov8KWFX82RrdRT_0" not in im_path:
+        #     continue
         im_fn = osp.basename(im_path)
         img = cv2.imread(im_path)
 
-        out_img = ht.inference(img=img, debug_plot=True)
+        out_img, h = ht.inference(img=img, debug_plot=True)
+        cv2.imwrite(
+            osp.join(
+                "/Users/dmitry/Initflow/doc-img-dewarping/segm_out",
+                osp.basename(im_path),
+            ),
+            out_img,
+        )
         cv2.imwrite(
             osp.join(
                 "/Users/dmitry/Initflow/doc-img-dewarping/segm_out_red_poly",
                 osp.basename(im_path),
             ),
-            out_img,
+            h,
         )
